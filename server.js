a3a20f05623da3983c2ab8a2f2880a957e451f6b
Ahmad Hani <Ahmad.hane899@gmail.com>
2022-10-12 21:56:49 +0300

    last update

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
-const express = require('express');
+import prisma, { Prisma }  from "@prisma/client"
+import express from "express"
+import {selectAll} from "./date.js"
+
 const app = express();
 app.use(express.json());
-
-const pharamcies = [
-    { id: 1 , name: 'alnoor', location: 'manama'},
-    { id: 2 , name: 'royan'},
-    { id: 3 , name: 'alhidd'},
-];
-
-
 app.set('view engine', 'ejs')
 
 app.get("/", (req, res) => {
@@ -16,30 +11,47 @@ app.get("/", (req, res) => {
     res.render("index", {text: 'World'})
 })
 
-app.get("/api/pharamcies", (req, res)=>
-{
-    res.send("pharamcies List")
-})
 
-app.get("/api/pharamcies/new", (req, res)=>
+async function main()
 {
-    res.send("pharmacy new form")
-})
+    const doctors = await prisma.doctor.findMany(
+        {
+        where: {
+            dID: 20220069,
+            },
+         }); 
+}
 
-app.get("/api/pharamcies/:id", (req, res)=>
+
+//pharmacy with ID
+app.get("/api/pharmacies/:id", (req, res)=>
 {
-    const pharmacy = pharamcies.find(c => c.id === parseInt(req.params.id));
+    const pharmacy = pharmacies.find(m => m.id === parseInt(req.params.id));
     if (!pharmacy) res.status(404).send('the pharmacy with the given ID was not found');
     res.send(pharmacy);
 })
 
+//medicines
+app.get("/api/medicines/", (req, res)=>
+{
+    res.send(Medicines)
+})
+
+//medicines with ID
+app.get("/api/medicines/:id", (req, res)=>
+{
+    const medicine = Medicines.find(m => m.id === parseInt(req.params.id));
+    if (!medicine) res.status(404).send('the medicine with the given ID was not found');
+    res.send(medicine);
+})
+
 //posts for pharmacies
-app.post('/api/pharamcies', (req, res) =>{
+app.post('/api/pharmacies', (req, res) =>{
     const pharmacy = {
-        id: pharamcies.length +1,
+        id: pharmacies.length +1,
         name: req.body.name
     }
-    pharamcies.push(pharmacy);
+    pharmacies.push(pharmacy);
     res.send(pharmacy);
 })
 
@@ -47,3 +59,34 @@ app.post('/api/pharamcies', (req, res) =>{
 
 app.listen(8000, console.log('lisening on port 8000....'))
 
+
+
+
+/*End Points:
+•	Medicines
+o	ID
+o	Name
+•	Customer
+o	ID
+•	Doctor
+o	ID
+•	Order
+o	ID
+•	Staff
+o	ID
+•	Admin
+o	ID
+ */
+
+/*const pharmacies = [
+    { id: 1 , name: 'alnoor', location: 'manama'},
+    { id: 2 , name: 'royan'},
+    { id: 3 , name: 'alhidd'},
+];
+
+const Medicines = [
+    { id: 1 , name: 'panadol'},
+    { id: 2 , name: 'aminor'},
+    { id: 3 , name: 'airon'},
+];
+*/
